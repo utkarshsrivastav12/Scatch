637df8f09a05a5d6680ad5d8c623106775b8a3a8
utkarshsrivastav12 <theutkarsh5@gmail.com>
2024-06-01 17:27:47 +0530

    intial setup

---
+++ config/mongoose-connection.js
@@ -0,0 +1,14 @@
+const { log } = require("console");
+const mongoose = require("mongoose");
+
+mongoose
+.connect("mongodb://127.0.0.1:27017/scatch")
+.then(function(){
+    console.log("connected")
+})
+.catch(function(err){
+    console.log(err);
+})
+
+module.exports = mongoose.connection;
+
